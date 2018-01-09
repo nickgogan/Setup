82af99f1d06ac773c9a04293b66db0de101c5b2b
nickgogan <gogannick@gmail.com>
2018-01-09 18:36:05 -0500

    APIs: Start implementing Express middleware.

--- app_APIs/src/app/app.js
+++ app_APIs/src/app/app.js
@@ -1,11 +1,42 @@
+// @ts-check
+
 import express from 'express';
+// import { passingLog, errorLog } from './server/helpers';
+import * as helpers from './server/helpers';
 
 const app = express();
 
+// Middlewares
+// - Log handlers
+app.use(helpers.outputLog);
+app.use(helpers.errorLog);
+
+// - Error handlers
 app.use((req, res, next) => {
   res.status(200).json({
     message: 'Middleware works!'
   });
+});
+
+// app.use(helpers.resourceMissing)
+app.use((req, res, next) => {
+  const error = new Error('Not found...');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  // Respond to client
+  const err = app.get('env') === 'development' ? error : {};
+  const status = err.status || 500;
+
+  res.status(status).json({
+    error: {
+      message: err.message
+    }
+  });
+  // Respond to myself
+  console.log(error);
 });
 
 export default app;
