155d910ab05339601b6466c689f1e8f618b6b643
nickgogan <gogannick@gmail.com>
2018-01-09 21:56:03 -0500

    APIs: add env support.

--- app_APIs/src/app/server/server.js
+++ app_APIs/src/app/server/server.js
@@ -1,9 +1,16 @@
 // @ts-check
 
 import http from 'http';
+import * as dotEnv from 'dotenv-safe';
 import app from '../app';
 
-const port = process.env.PORT || 3001;
+const env = dotEnv.load({
+  path: 'src/app/server/env/.env',
+  sample: 'src/app/server/env/.env.example',
+  allowEmptyValues: true
+});
+
+const port = env.PORT || 3001;
 const server = http.createServer(app);
 
 server.listen(port);
