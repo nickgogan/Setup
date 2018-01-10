c7adaab63ca049d71e7abcef1c5c290f6720ed3b
nickgogan <gogannick@gmail.com>
2018-01-10 13:58:51 -0500

    APIs: Start adding some product-specific endpoints.

--- app_APIs/src/app/server/routes/products.js
+++ app_APIs/src/app/server/routes/products.js
@@ -4,4 +4,16 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {});
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'GET products'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'POST products'
+  });
+});
+
+export default router;
