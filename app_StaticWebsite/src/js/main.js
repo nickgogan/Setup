a65e0710d82de7f45afecdec302f024d097a0d0c
nickgogan <gogannick@gmail.com>
2018-01-18 21:14:55 -0500

    Static Web Site: Found issue with dynamizally-imported js files. Will investigate futher.

--- app_StaticWebsite/src/js/main.js
+++ app_StaticWebsite/src/js/main.js
@@ -2,7 +2,9 @@
 // @flow
 
 import ConsoleLogHTML from 'console-log-html';
-import test from './test';
+import test from './components/test';
+
+import '../styles/main.css';
 
 ConsoleLogHTML.connect(document.querySelector('#log'));
 
@@ -10,13 +12,13 @@ console.log('Hello from main!');
 console.log(test());
 
 // TODO: Investigate possibility of using async/await here.
-Promise.all([
-  import(/* webpackChunkName: "foo" */ './components/foo'),
-  import(/* webpackChunkName: "bar" */ './components/bar')
-])
-  .then(([{ Foo }, { Bar }]) => {
-    console.log(`Lazy-loaded modules: Foo: ${Foo()}, Bar: ${Bar()}`);
-  })
-  .catch(e => {
-    console.error(e);
-  });
+// Promise.all([
+//   import(/* webpackChunkName: "foo" */ './components/foo'),
+//   import(/* webpackChunkName: "bar" */ './components/bar')
+// ])
+//   .then(([{ Foo }, { Bar }]) => {
+//     console.log(`Lazy-loaded modules:${  Foo()}`); // , Bar: ${Bar()}`);
+//   })
+//   .catch(e => {
+//     console.error(e);
+//   });
