3a0d88ebe6f334b2cdcdd7a82aaec04da28c3891
nickgogan <gogannick@gmail.com>
2018-01-24 13:15:47 -0500

    Static Web Site: Add robots generator and favicon generator.

--- app_StaticWebsite/src/webpack/parts/prod/templatesLoader.babel.js
+++ app_StaticWebsite/src/webpack/parts/prod/templatesLoader.babel.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import WebpackHtmlPlugin from 'html-webpack-plugin'; // eslint-disable-line
 import WebpackHtmlHarddiskPlugin from 'html-webpack-harddisk-plugin'; // eslint-disable-line
+import WebpackGenerateRobotsPlugin from 'robotstxt-webpack-plugin'; // eslint-disable-line
+import WebpackFaviconsGenerator from 'favicons-webpack-plugin';
 
 export default () => {
   const htmlToHdd = new WebpackHtmlHarddiskPlugin({
@@ -13,11 +15,41 @@ export default () => {
     inject: 'body'
     // alwaysWriteToDisk: true // htmlToHdd should handle this.
   });
+  const generateRobots = new WebpackGenerateRobotsPlugin({
+    policy: [
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: '/search',
+        crawlDelay: 2
+      },
+      {
+        userAgent: 'OtherBot',
+        allow: ['/allow-for-all-bots', '/allow-only-for-other-bot'],
+        disallow: ['/admin', '/login'],
+        crawlDelay: 2
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/search',
+        crawlDelay: 10,
+        cleanParam: 'ref /articles/'
+      }
+    ]
+  });
+  const faviconsGenerator = new WebpackFaviconsGenerator({
+    logo: path.resolve(__dirname, '../../../assets/favicon.png'),
+    title: 'My App',
+    persistentCache: true,
+    inject: true,
+    background: '#fff'
+  });
 
   return {
     module: {
       rules: []
     },
-    plugins: [htmlIndex, htmlToHdd]
+    plugins: [htmlIndex, htmlToHdd, generateRobots, faviconsGenerator]
   };
 };
