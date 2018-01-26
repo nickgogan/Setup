570abcb9961d93c2e727d8ecf690c73f749bef3c
nickgogan <gogannick@gmail.com>
2018-01-26 13:52:47 -0500

    Static Web Site: Cleanup and added bundle splitting with async and caching. Also added build caching.

--- app_StaticWebsite/src/webpack/webpack.config.prod.babel.js
+++ app_StaticWebsite/src/webpack/webpack.config.prod.babel.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import webpack from 'webpack';
-import WebpackMerge from 'webpack-merge';
 import dotEnv from 'dotenv-safe';
-import DotenvWebpackPlugin from 'dotenv-webpack';
-import WebpackMonitorPlugin from 'webpack-monitor';
-import WebpackGitRevisionPlugin from 'git-revision-webpack-plugin';
-import WebpackCompressionPlugin from 'compression-webpack-plugin';
+import MergePlugin from 'webpack-merge';
+import DotenvPlugin from 'dotenv-webpack';
+import MonitorPlugin from 'webpack-monitor';
+import BundleAnalyzerPlugin from 'webpack-bundle-analyzer';
+import GitRevisionPlugin from 'git-revision-webpack-plugin';
+import CompressionPlugin from 'compression-webpack-plugin';
+import CacheBundles from 'hard-source-webpack-plugin';
 
 /*
 ########################################
@@ -26,7 +28,7 @@ import extractBundles from './parts/prod/extractBundles.babel';
 // Needed for use in this config file.
 const env = dotEnv.load({
   path: path.resolve(__dirname, './../env/prod.env'),
-  sample: path.resolve(__dirname, './../env/prod.example.env')
+  sample: path.resolve(__dirname, './../env/prod.example.env'),
 }).parsed;
 env.OUT_FULL_PATH = path.resolve(__dirname, '../../', env.DEST);
 const ENV = Object.assign({}, common.PATHS, env);
@@ -37,21 +39,22 @@ const ENV = Object.assign({}, common.PATHS, env);
 ########################################
 */
 // Webpack sets the app-wide process.env.* variables.
-const dotEnvWebpack = new DotenvWebpackPlugin({
+const appEnv = new DotenvPlugin({
   path: path.join(__dirname, '../env/prod.env'),
-  safe: false
+  safe: false,
 });
-const webpackMonitor = new WebpackMonitorPlugin({
+const webpackMonitor = new MonitorPlugin({
   capture: true,
   launch: true,
-  port: 3000
+  port: 3000,
 });
-const webpackCompression = new WebpackCompressionPlugin({
+const webpackCompression = new CompressionPlugin({
   test: /\.js($|\?)/i,
-  deleteOriginalAssets: true
+  deleteOriginalAssets: true,
 });
+const webpackModuleConcatenator = new webpack.optimize.ModuleConcatenationPlugin();
 const webpackBanner = new webpack.BannerPlugin({
-  banner: new WebpackGitRevisionPlugin().version()
+  banner: new GitRevisionPlugin().version(),
 });
 
 /*
@@ -59,7 +62,7 @@ const webpackBanner = new webpack.BannerPlugin({
               Exported Webpack Config
 ########################################
 */
-export default WebpackMerge(
+export default MergePlugin(
   common.config,
   loadBabel(),
   loadTemplates(), // This has to come first to get Critical CSS working.
@@ -67,20 +70,34 @@ export default WebpackMerge(
   extractBundles([
     {
       name: 'react',
-      minChunks: ({ resource }) => /node_modules/.test(resource) // Only pull in the used code form node_modules.
-    }
+      minChunks: ({ resource }) => /node_modules/.test(resource), // Only pull in that used code from node_modules.
+    },
+    {
+      name: 'WebpackRuntime',
+      minChunks: Infinity,
+    },
+    //
+    {
+      async: true,
+      children: true,
+      name: 'CommonLazy',
+    },
   ]),
   {
-    // Bundles to split off from main (which is in webpack.common.js)
-    entry: {
-      react: ['react']
-    },
     output: {
       path: ENV.OUT_FULL_PATH,
-      filename: `[name].js`,
-      chunkFilename: `[name].v${ENV.VERSION}.js`
+      filename: `[name].[chunkhash:8].js`,
+      chunkFilename: `[name].v${ENV.VERSION}.js`,
       // publicPath: OUT_PATH
     },
-    plugins: [dotEnvWebpack, webpackCompression, webpackMonitor] // , webpackMonitor
-  }
+    plugins: [
+      appEnv,
+      webpackBanner,
+      webpackModuleConcatenator,
+      // webpackCompression,
+      new CacheBundles(),
+      // webpackMonitor
+      // new BundleAnalyzerPlugin.BundleAnalyzerPlugin()
+    ],
+  },
 );
